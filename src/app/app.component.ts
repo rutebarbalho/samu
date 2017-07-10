081c56f4edfdd55c43f242752ffd9bd13e134e28
rutebarbalho <barbalho.rute@gmail.com>
2017-07-10 11:41:37 -0300

    samu-incompleto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,13 @@ import {SamuService} from './services/samu.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UFService, SamuService]
 })
 export class AppComponent implements OnInit {
     title = 'app';
+    uf_id = '';
+    area = '';
     ufs : UF[];
     dados_da_samu : Dados[];
 
@@ -22,5 +25,18 @@ export class AppComponent implements OnInit {
     ngOnInit(): void {
         this.ufs = this.ufService.getAll();
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
+        this.defineTitle();
+        //this.detailUF();
     }
-}
+    defineTitle(): void {
+      for(let uf of this.ufs){
+        if(uf.id == 42) this.title = uf.nome;
+      }
+
+      //detailUF(): void{
+        //for(let uf of this.ufs){
+          //if(uf.id == 42)
+      //this.uf_id = String(uf.id);
+      //this.area = String(uf.area);
+    }
+  }
